c6fd160c76832e7b2d3ed1928ac47c32760af3cb
jemunro <jacob.e.munro@gmail.com>
2019-02-15 15:20:04 +1100

    update

--- src/main/groovy/tidynf/TidyHelpers.groovy
+++ src/main/groovy/tidynf/TidyHelpers.groovy
@@ -4,7 +4,7 @@ import java.nio.file.Path
 
 class TidyHelpers {
 
-    static List as_file(ArrayList strings) {
+    static List as_file(List strings) {
         strings.collect { as_file(it.toString()) }
     }
 
@@ -12,27 +12,52 @@ class TidyHelpers {
         new File(string).toPath().toAbsolutePath()
     }
 
-    static List file_ext(List paths, String ext) {
-        paths.collect { file_ext(it as Path, ext) }
+    static Object file_ext(List object, String ext){
+         if (object instanceof Path){
+             file_ext_path(object, ext)
+         } else {
+             file_ext_list(object, ext)
+         }
     }
 
-    static Path file_ext(Path path, String ext) {
+    static List file_ext_list(List paths, String ext) {
+        paths.collect { file_ext_path(it as Path, ext) }
+    }
+
+    static Path file_ext_path(Path path, String ext) {
         new File(path.toString() + ext).toPath().toAbsolutePath()
     }
 
-    static List file_replace(List paths, String pattern, String replacement) {
-        paths.collect { file_replace(it as Path, pattern, replacement) }
+    static Object file_replace(List object, String pattern, String replacement) {
+        if (object instanceof Path){
+            file_replace_path(object, pattern ,replacement)
+        } else {
+            file_replace_list(object, pattern ,replacement)
+        }
     }
 
-    static Path file_replace(Path path, String pattern, String replacement) {
+    static List file_replace_list(List paths, String pattern, String replacement) {
+        paths.collect { file_replac_path(it as Path, pattern, replacement) }
+    }
+
+    static Path file_replace_path(Path path, String pattern, String replacement) {
         new File(path.toString().replaceAll(pattern, replacement)).toPath().toAbsolutePath()
     }
 
-    static Float file_size(List files, String units = 'GB'){
-        files.collect { file_size(it as Path, units) }.sum() as Float
+    static Float file_size(List object, String units = 'GB') {
+        if (object instanceof Path){
+            file_size_path(object, units)
+        } else {
+            file_size_list(object, units)
+        }
     }
 
-    static Float file_size(Path path, String units = 'GB'){
+    static Float file_size_list(List files, String units = 'GB') {
+        files.collect { file_size_path(it as Path, units) }.sum() as Float
+    }
+
+
+    static Float file_size_path(Path path, String units = 'GB'){
         def length = new File(path.toString()).length()
         def unit_defs = [B:1, KB:1e3, MB:1e6, GB:1e9]
         if (unit_defs.containsKey(units)){
